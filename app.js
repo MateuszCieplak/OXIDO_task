96b447a8885f93bd9b21536474a44f735441bc15
Mateusz Cieplak <m.cieplak97@gmail.com>
2024-11-15 20:31:24 +0100

    Stworzenie funkcji odczytującej plik tekstowy

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const fs = require('fs');
 
 //Klucz API OpenAI
 const OPENAI_API_KEY = "YOUR_OPENAI_API_KEY";
+
+// Funkcja która odczytuje pliku tekstowego
+function czytajPlik(filepath) {
+    return fs.readFileSync(filepath, 'utf8');
+}
